372942463f57565b592c36fff60e9b25a8659de0
Chanwoo Kim <chanwoohill@gmail.com>
2016-08-03 22:36:37 +0000

    change to using ar

--- contact.rb
+++ contact.rb
@@ -1,74 +1,29 @@
-class Contact
+require 'active_record'
 
-  attr_accessor :name, :email
-  attr_reader :id 
+class Contact < ActiveRecord::Base
 
-  def initialize(name, email, id=nil)
-    @name = name 
-    @email = email
-    @id = id
-  end
-
-  def to_s
-    "#{@id}: #{@name}, #{@email}"
-  end
-
-  #Write/update and save the contacts to a new row in the database 
-  def save
-    if id 
-      Contact.connection.exec_params('UPDATE contacts SET name = $1, email = $2 WHERE id = $3::int;', [name, email, id])
-    else 
-      result = Contact.connection.exec_params('INSERT INTO contacts (name, email) VALUES ($1, $2) RETURNING id;', [name, email])
-      @id = result[0]['id']
-    end 
-  end
-
-  #Delete a contract 
-  def destroy
-    Contact.connection.exec_params(
-     'DELETE FROM contacts WHERE id = $1',
-     [id])
-  end
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, length: { minimum: 5 } 
 
   class << self
 
-  @@connection = nil
-  def connection
-    @@connection || PG::Connection.open(dbname: 'contacts')
-  end
+    def search(term)
+      self.where("name ILIKE ? OR email ILIKE ?", "%#{term}%","%#{term}%").all
+    end 
 
-  #Select all the contacts from the database 
-  def all
-    result = connection.exec_params('SELECT * FROM contacts ORDER BY id;')
-    result.map do |row|
-      Contact.new(row['name'], row['email'], row['id'])
+    def to_s
+      "ID: #{id} Name: #{name} Email: #{email}"
     end
+
   end
 
-  #Find a contact by id and display its details 
-  def find(id)
-    begin 
-      result = connection.exec_params('SELECT * FROM contacts WHERE id = $1::int;', [id])
-      Contact.new(result[0]['name'], result[0]['email'], result[0]['id'])
-    rescue IndexError
-      nil
-    end
+  def details
+    "ID: #{id}\nName: #{name}\nEmail: #{email}"
   end
-
-  #Search for contact name or email by term 
-  def search(term)
-    result = connection.exec_params('SELECT * FROM contacts WHERE name ILIKE $1 OR email ILIKE $1 ORDER BY id;', ["%#{term}%"])
-    contacts = result.map do |row|
-      Contact.new(row['name'], row['email'], row['id'])
-    end
-  end
-
-
-
-  end 
 
 end 
  
+
 
 
 
--- contact_list.rb
+++ contact_list.rb
@@ -1,89 +1,107 @@
+require 'active_record'
 require_relative 'contact'
-require 'pg'
+
 
 class ContactList
 
-  class NoContactFound < StandardError
-  end
-
-  def initialize
-    @command, @parameter = ARGV
+  def initialize  
+    ActiveRecord::Base.establish_connection(
+      :adapter => "postgresql",
+      :database => "contacts")
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
   def begin
-    case @command
-    when "help" then menu
-    when "new"  then create_contact 
-    when "list" then list_contacts(Contact.all)
-    when "show" then find_contact(@parameter)
-    when "search" then list_contacts(Contact.search(@parameter))
-    when "update" then update_contact(@parameter)
-    when "destroy" then destroy_contact(@parameter)
+
+    while true do
+      menu
+      case input
+      when "help" 
+        menu
+      when "new"  
+        contact = Contact.new
+        puts "Name of the contact?"
+        contact.name = input
+        puts "email?"
+        contact.email = input
+
+        if contact.save
+          puts contact
+        else
+          puts contact.errors.messages
+        end
+      when "list" 
+        contacts = Contact.all
+        puts contacts
+      when "show" 
+        puts "what is contact id?"
+        id = gets.strip
+        contact = Contact.find(id)
+          if contact.details
+            puts contact
+          else
+            puts "Not found"
+          end
+      when "search"  
+        puts "Search term?"
+        term = input
+        contact = Contact.search(term)
+          puts contact
+      when "update" 
+        puts "what is contact id?"
+        id = gets.strip
+        contact = Contact.find(id)
+        puts "Which field do you want to edit [name, email]?"
+        field = input
+        puts "New Value?"
+        value = input
+
+        begin
+          contact.update!(field => value)
+        rescue ActiveRecord::RecordInvalid => ex
+          puts contact.errors.messages
+        end  
+      when "destroy" 
+        puts "what is contact id?"
+        id = gets.strip
+        contact = Contact.find(id)
+        contact.destroy 
+      when "quit"
+        break
+      else
+        puts "Try again"
+      end
     end
+    puts "Good Bye!"
   end
 
-  def find_contact(id)
-    begin
-      contact = Contact.find(id)
-      raise NoContactFound if contact.nil?
-      puts "Name: #{contact.name}, Email: #{contact.email}"
-    rescue NoContactFound
-      puts "There is no contact with id ##{id}."
+    def input
+      while true do
+        print "> "
+        v = gets.strip
+        if v.length > 0
+          break
+        else
+          puts "invalid input"
+        end
+      end
+      v
     end
-  end
 
-  def update_contact(id)
-    contact = Contact.find(id)
-    puts "Which field do you want to edit [name, email]?"
-    field = $stdin.gets.chomp
-    puts "New Value?"
-    value = $stdin.gets.chomp
-    contact.send("#{field}=", value)
-    contact.save 
-  end 
-
-  def create_contact
-    name = prompt_name
-    email = prompt_email
-    contact = Contact.new(name, email)
-    contact.save
-  end
-
-  def destroy_contact(id)
-    contact = Contact.find(id)
-    contact.destroy
-  end 
-
-  def prompt_name
-    print "Enter new contact's name: "
-    $stdin.gets.chomp
-  end
-
-  def prompt_email
-    print "Enter new contact's e-mail: "
-    $stdin.gets.chomp
-  end
-
-  def list_contacts(contacts)
-    contacts.each do |contact|
-      puts contact.to_s
+    def menu
+      puts "Here is a list of available commands:"
+      puts "\thelp - Open menu"
+      puts "\tdestroy - Delete a contact"
+      puts "\tnew - Create a new contact"
+      puts "\tlist - List all contacts"
+      puts "\tshow - Show a contact"
+      puts "\tsearch - Search contacts"
+      puts "\tupdate - Edit contacts"
+      puts "\tquit - Exit"
     end
-    puts "---"
-    puts "#{contacts.size} records total"
-  end
-
-  def menu
-    puts "Here is a list of available commands:"
-    puts "\tdestroy - Delete a contact"
-    puts "\tnew - Create a new contact"
-    puts "\tlist - List all contacts"
-    puts "\tshow - Show a contact"
-    puts "\tsearch - Search contacts"
-    puts "\tupdate - Edit contacts"
-  end
 
 end
 
-
+ContactList.new.begin
 
 
